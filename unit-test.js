53c12190bed2beec416b942345cc8757a471287e
Matthew Nau <matthewnau@users.noreply.github.com>
2018-06-09 16:54:03 -0400

    updated package
    removed blank line from package.json. Also, removed unit tests since they all passed

--- unit-test.js
+++
@@ -1,9 +0,0 @@
-// require the libmoji library
-const libmoji = require('../../Github/libmoji/libmoji.js');
-
-//console.log(libmoji.getComicId(libmoji.randTemplate(libmoji.templates)));
-
-let url = `https://render.bitstrips.com/v2/cpanel/8968038-316830037_35-s5-v1.png?transparent=1&palette=1`
-let id = libmoji.getAvatarId(url);
-let comic = libmoji.getComicId(libmoji.randTemplate(libmoji.templates));
-console.log(libmoji.buildComicUrl(comic, id, 0, 2));
